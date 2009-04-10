50bf63cad4f755fc4f016a1b20159d35a6852eef
Seiji Toyama <seijit@me.com>
2009-04-10 18:48:37 +0900

    second

---
+++ rsync.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+LIST=list.txt
+SRC=~/
+DEST=~/env/
+RSYNC=/opt/local/bin/rsync
+
+for x in `cat $LIST|grep -v "^#"`
+do
+    echo "--- $SRC$x => $DEST$x ---"
+   $RSYNC -avz $SRC$x $DEST$x
+done
